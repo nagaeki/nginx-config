0ad0650d43b23a6bdf313b1788ccd37eeaa1fefa
eki <m@eki.moe>
2023-06-15 19:23:16 +0900

    fix

--- install.sh
+++ install.sh
@@ -39,8 +39,8 @@ if [ ! -d "/etc/nginx/" ]; then
 echo "Error: Nginx Install failed."
 exit 1
 else
-	curl -sS https://github.com/nagaeki/nginx-conf/raw/main/nginx.conf > /etc/nginx/nginx.conf
-	curl -sS https://github.com/nagaeki/nginx-conf/raw/main/default.conf > /etc/nginx/conf.d/default.conf
+	wget https://github.com/nagaeki/nginx-conf/raw/main/nginx.conf -O /etc/nginx/nginx.conf
+	wget https://github.com/nagaeki/nginx-conf/raw/main/default.conf -O /etc/nginx/conf.d/default.conf
 fi
 
 # Install ACME.SH
@@ -49,4 +49,4 @@ curl https://get.acme.sh | sh
 fi
 
 # Install dhparam 
-curl -sS https://github.com/internetstandards/dhe_groups/raw/main/ffdhe4096.pem > /etc/nginx/ffdhe4096.pem
+wget https://github.com/internetstandards/dhe_groups/raw/main/ffdhe4096.pem -O /etc/nginx/ffdhe4096.pem
